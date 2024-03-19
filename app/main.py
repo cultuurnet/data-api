558f5c9bcc0dcda330b4b872aeea0690166f93e3
Jonny Daenen <jonnydaenen@gmail.com>
2024-03-19 17:15:43 +0100

    Add bq endpoint

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ import requests
 from fastapi import FastAPI, Query
 from pyproj import CRS, Transformer
 from shapely.geometry import Point
+from fastapi import HTTPException
 
 from app.secretmanager import Config
 
@@ -23,51 +24,57 @@ statsector_parquet = impresources.files(data) / 'statistical_sectors_2023.parque
 
 app = FastAPI()
 
+
 # Cache this so the file is only read once (during lifecyle of container)
 @cache
 def get_statsectors():
-    logger.info('reading file...')
+    logger.info("reading file...")
     return gpd.read_parquet(statsector_parquet)
+
 
 @app.get("/", tags=["root"])
 async def root():
     return {"message": "Statsector API"}
 
+
 @app.get("/get-statsector/")
 async def get_statsector(
     lat: float = Query(None, description="Latitude"),
     lon: float = Query(None, description="Longitude"),
-    address: str = Query(None, description="Address")
+    address: str = Query(None, description="Address"),
 ):
     if lat is None and lon is None:
         if address is not None:
             # Additional validation: Ensure address is a string
             if not isinstance(address, str):
                 return {"error": "'address' must be of type string."}
-            
+
             base_url = "https://maps.googleapis.com/maps/api/geocode/json"
-            
+
             params = {
                 "address": address,
                 "key": api_key,
             }
-            
+
             try:
                 response = requests.get(base_url, params=params)
                 data = response.json()
-                
+
                 if response.status_code == 200:
                     if data["status"] == "OK":
                         location = data["results"][0]["geometry"]["location"]
                         lat = location["lat"]
                         lon = location["lng"]
                     else:
-                        return {"error": f"Geocoding API response status: {data['status']}"}
+                        return {
+                            "error": f"Geocoding API response status: {data['status']}"
+                        }
                 else:
-                    return {"error": f"Request failed with status code: {response.status_code}"}
+                    return {
+                        "error": f"Request failed with status code: {response.status_code}"
+                    }
             except Exception as e:
                 return {"error": f"An error occurred: {e}"}
-
 
         else:
             return {"error": "Either 'lat' and 'lon' or 'address' must be provided."}
@@ -75,7 +82,7 @@ async def get_statsector(
         if not isinstance(lat, float) or not isinstance(lon, float):
             return {"error": "'lat' and 'lon' must be of type float."}
 
-    # Stat Sector uses Lambert projection 
+    # Stat Sector uses Lambert projection
     # So we have to have transformers, World Geodetic System to Lambert and back
     # https://www.ngi.be/website/de-lambert-kaartprojectie-2/
     crs_lambert = CRS.from_string("EPSG:31370")
@@ -93,14 +100,61 @@ async def get_statsector(
 
     try:
         # Look up coordinates in the file
-        sector_results = statsectors[statsectors['geometry'].contains(lam_coo)]
+        sector_results = statsectors[statsectors["geometry"].contains(lam_coo)]
         first_sector_result = sector_results.iloc[0]
 
-        response = {"sector_id": first_sector_result['cd_sector'], "sector_name": first_sector_result['tx_sector_descr_nl']}
-        
-        if first_sector_result['cd_sector']:
+        response = {
+            "sector_id": first_sector_result["cd_sector"],
+            "sector_name": first_sector_result["tx_sector_descr_nl"],
+        }
+
+        if first_sector_result["cd_sector"]:
             return response
         else:
             return {"error": "Sector not found for the given coordinates"}
     except Exception as e:
         return {"error": str(e)}
+
+
+@app.post("/get-statsector-bq/")
+async def get_statsector_bq(payload: dict):
+    """Example payload
+    {
+        "requestId": "124ab1c",
+        "caller": "//bigquery.googleapis.com/projects/myproject/jobs/myproject:US.bquxjob_5b4c112c_17961fafeaf",
+        "sessionUser": "test-user@test-company.com",
+        "userDefinedContext": {
+        "key1": "value1",
+        "key2": "v2"
+        },
+        "calls": [
+        [null, 1, "", "abc"],
+        ["abc", "9007199254740993", null, null]
+        ]
+    }
+
+    """
+    try:
+        mode = payload["userDefinedContext"]["mode"]
+        field = payload["userDefinedContext"]["field"]
+        if mode == "address":
+            results = []
+            for call in payload["calls"]:
+                result = await get_statsector(address=call[0])
+                results.append(result)
+            return results
+
+        elif mode == "coordinates":
+            results = []
+            for call in payload["calls"]:
+                result = await get_statsector(lat=call[0], lon=call[1])
+                results.append(result[field])
+            return results
+        else:
+            raise HTTPException(status_code=400, detail="Invalid mode")
+    except KeyError as e:
+        print(f'KeyError: {e}')
+        raise HTTPException(status_code=400, detail="Invalid payload format")
+    except Exception as e:
+        print(f'Exception: {e}')
+        raise HTTPException(status_code=500, detail=str(e))
